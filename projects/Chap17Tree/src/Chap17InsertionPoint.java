0b9ff670e3baf81e977f0bfacdc56b5bc5465ab5
Andy Knoblock <knoblock.andy@gmail.com>
2018-04-12 08:26:57 -0600

    Add files via upload

---
+++ projects/Chap17Tree/src/Chap17InsertionPoint.java
@@ -0,0 +1,16 @@
+
+public class Chap17InsertionPoint 
+{
+
+	public static void main(String[] args) 
+	{
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.add("Juliet"); 
+		tree.add("Tom");       
+		tree.add("Diana");  
+		tree.add("Harry");
+		tree.add("Romeo");
+		tree.print();
+	}
+
+}
